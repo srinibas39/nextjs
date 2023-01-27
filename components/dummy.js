e0504b9758719038c2932a2864253e1558248923
srinibas khuntia <srinibas.k@pillir.io>
2023-01-27 09:45:43 +0530

    added style jsx

---
+++ components/dummy.js
@@ -0,0 +1,17 @@
+const Dummy = () => {
+
+    return <>
+        <style jsx global>
+            {`
+      
+      .yellow{
+        background-color:yellow
+      }
+      `
+            }
+        </style>
+        <div className="yellow">I am dummy</div>
+    </>
+}
+
+export default Dummy
